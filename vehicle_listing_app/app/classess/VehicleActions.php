eb7cd7024e2350dd413521118dc3722658c81ed9
Sujoy Das <47408361+Sujoydas692@users.noreply.github.com>
2025-04-09 15:37:19 +0600

    Delete vehicle_listing_app/app/classess directory

--- vehicle_listing_app/app/classess/VehicleActions.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-interface VehicleActions {
-    public function addVehicle($data);
-    public function editVehicle($id, $data);
-    public function deleteVehicle($id);
-    public function getVehicles();
-}
